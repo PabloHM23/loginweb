5aed459f7f15f9e1ce844bc6e37fecaa27a24ecb
Pablo Hernández <141796387+PabloHM23@users.noreply.github.com>
2025-10-25 17:43:40 -0700

    php

---
+++ login/app/AuthController.php
@@ -0,0 +1,13 @@
+<?php 
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if($username == "pablohzm"){
+    if($password == "123123"){
+        ('Location: ../home.html')
+        
+    }
+}
+
+?>
